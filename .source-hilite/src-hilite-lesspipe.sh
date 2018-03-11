b09b7b5dc773e923ccbf738afcf86d16cbca536c
Sho Hashimoto <hashimoto@shokai.org>
2018-03-11 16:38:44 +0900

    fix less-highlight indent

--- .source-hilite/src-hilite-lesspipe.sh
+++ .source-hilite/src-hilite-lesspipe.sh
@@ -2,10 +2,11 @@
 
 for source in "$@"; do
     case $source in
-	*ChangeLog|*changelog) 
-        source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HOME/.source-hilite/shokai.style -i "$source" ;;
-	*Makefile|*makefile) 
-        source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HOME/.source-hilite/shokai.style -i "$source" ;;
-        *) source-highlight --failsafe --infer-lang -f esc --style-file=$HOME/.source-hilite/shokai.style -i "$source" ;;
+	    *ChangeLog|*changelog)
+            source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HOME/.source-hilite/shokai.style -i "$source" ;;
+	    *Makefile|*makefile)
+            source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HOME/.source-hilite/shokai.style -i "$source" ;;
+        *)
+            source-highlight --failsafe --infer-lang -f esc --style-file=$HOME/.source-hilite/shokai.style -i "$source" ;;
     esac
 done
